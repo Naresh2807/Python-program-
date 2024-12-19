6c86160ba6bc4fc8a3aae5e3dbd165691811eadc
Naresh krishnan <127166960+Naresh2807@users.noreply.github.com>
2024-12-19 08:54:03 +0530

    Add files via upload

---
+++ password.py
@@ -0,0 +1,7 @@
+import re
+
+password = input("Enter the password: ")
+if re.fullmatch(r'(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}', password):  
+    print("valid.")
+else:
+	print("not valid")
