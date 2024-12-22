747ad5d839cf83341bd4f09465e2d1517c215a3d
Naresh krishnan <127166960+Naresh2807@users.noreply.github.com>
2024-12-22 12:19:12 +0530

    Update password.py

--- password.py
+++ password.py
@@ -4,4 +4,4 @@ password = input("Enter the password: ")
 if re.fullmatch(r'(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}', password):  
     print("valid.")
 else:
-	print("not valid")
+    print("not valid")
